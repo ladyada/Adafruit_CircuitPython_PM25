3b81d49ad42a34c26c6eb64ebc6006f400164c70
lady ada <limor@ladyada.net>
2020-05-23 14:18:09 -0400

    i2c workin'

--- adafruit_pm25.py
+++ adafruit_pm25.py
@@ -33,21 +33,96 @@ Implementation Notes
 
 **Hardware:**
 
-.. todo:: Add links to any specific hardware product page(s), or category page(s). Use unordered list & hyperlink rST
-   inline format: "* `Link Text <url>`_"
+Works with most (any?) Plantower UART or I2C interfaced PM2.5 sensor.
 
 **Software and Dependencies:**
 
 * Adafruit CircuitPython firmware for the supported boards:
   https://github.com/adafruit/circuitpython/releases
+* Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
 
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies based on the library's use of either.
-
-# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
-# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
 # imports
+from adafruit_bus_device.i2c_device import I2CDevice
+from digitalio import DigitalInOut, Direction, Pull
+from micropython import const
+import struct
+import time
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_PM25.git"
+
+class PM25_I2C:
+    """
+    A driver for the PM2.5 Air quality sensor over I2C
+    """
+
+    def __init__(self, i2c_bus, reset_pin=None, address=0x12):
+        if reset_pin:
+            # Reset device
+            reset_pin.direction = Direction.OUTPUT
+            reset_pin.value = False
+            time.sleep(0.01)
+            reset_pin.value = True
+            # it takes at least a second to start up
+            time.sleep(1)
+
+        for _ in range(5): # try a few times, it can be sluggish
+            try:
+                self.i2c_device = I2CDevice(i2c_bus, address)
+                break
+            except ValueError:
+                time.sleep(1)
+                continue
+        else:
+            raise RuntimeError("Unable to find PM2.5 device")
+        # rad, ok make our internal buffer!
+        self._buffer = bytearray(32)
+        self.aqi_reading = {
+            "pm10 standard": None,
+            "pm25 standard": None,
+            "pm100 standard": None,
+            "pm10 env": None,
+            "pm25 env": None,
+            "pm100 env": None,
+            "particles 03um": None,
+            "particles 05um": None,
+            "particles 10um": None,
+            "particles 25um": None,
+            "particles 50um": None,            
+            "particles 100um": None,
+        }
+
+    def _read_into_buffer(self):
+        with self.i2c_device as i2c:
+            try:
+                i2c.readinto(self._buffer)
+            except OSError as e:
+                raise RuntimeError("Unable to read from PM2.5 over I2C")
+
+    def read(self):
+        self._read_into_buffer()
+        #print([hex(i) for i in self._buffer])
+
+        # check packet header
+        if (self._buffer[0] != 0x42) or (self._buffer[1] != 0x4D):
+            raise RuntimeError("Invalid PM2.5 header")
+
+        # check frame length
+        frame_len = struct.unpack(">H", self._buffer[2:4])[0]
+        if frame_len != 28:
+            raise RuntimeError("Invalid PM2.5 frame length")
+
+        checksum = struct.unpack(">H", self._buffer[30:32])[0]
+        check = sum(self._buffer[0:30]) 
+        if check != checksum:
+            raise RuntimeError("Invalid PM2.5 checksum")
+        
+        # unpack data
+        frame = struct.unpack(">HHHHHHHHHHHH", self._buffer[4:28])
+        self.aqi_reading["pm10 standard"], self.aqi_reading["pm25 standard"], self.aqi_reading["pm100 standard"], self.aqi_reading["pm10 env"], \
+        self.aqi_reading["pm25 env"], self.aqi_reading["pm100 env"], self.aqi_reading["particles 03um"], self.aqi_reading["particles 05um"], self.aqi_reading["particles 10um"], \
+        self.aqi_reading["particles 25um"], self.aqi_reading["particles 50um"], self.aqi_reading["particles 100um"] = frame
+
+        return self.aqi_reading
